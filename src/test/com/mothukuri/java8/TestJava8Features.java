2afd80e7b97a60e7f812b08f5d4b1ee169140796
mothukuri <nmothukuri@gmail.com>
2023-08-28 06:29:00 -0400

    .

--- src/test/com/mothukuri/java8/TestJava8Features.java
+++
@@ -1,9 +0,0 @@
-package com.mothukuri.java8;
-
-public class TestJava8Features {
-    @Test
-    void testStreamFunction(){
-
-    }
-    
-}
--- src/test/java/com/mothukuri/java8/TestJava8Features.java
+++ src/test/java/com/mothukuri/java8/TestJava8Features.java
@@ -1,9 +1,10 @@
 package com.mothukuri.java8;
 
+import org.junit.jupiter.api.Test;
+
 public class TestJava8Features {
     @Test
     void testStreamFunction(){
 
     }
-    
 }
